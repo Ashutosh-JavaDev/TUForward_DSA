9f2b12adcdd6c78d639130e49ed2ccbddaf2b1b6
Ashutosh Ayush <jbnashu297@gmail.com>
2025-11-06 14:17:15 +0530

    Second Largest Number

--- Array/src/SecondMax.java
+++ Array/src/SecondMax.java
@@ -1,19 +1,23 @@
 public class SecondMax {
-    static int second(int arr[]){
-        int secmax=arr[0];
-        int max=arr[0];
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>max){
-                max=arr[i];
+    static int second(int arr[]) {
+        int secmax = arr[0];
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+
             }
-          
+            else if(arr[i]>secmax){
+                secmax=arr[i];
+            }
 
         }
         return secmax;
     }
+
     public static void main(String[] args) {
-        int arr[]={32,12,53,2,42};
-        int res=second(arr);
+        int arr[] = { 32, 12, 33, 2, 42 };
+        int res = second(arr);
         System.out.println(res);
     }
 }
