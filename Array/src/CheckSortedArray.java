a7289f2afe78348b282750e0de46828b172feae9
Ashutosh Ayush <jbnashu297@gmail.com>
2025-11-06 23:21:00 +0530

    Java Sorted Array

--- Array/src/CheckSortedArray.java
+++ Array/src/CheckSortedArray.java
@@ -1,19 +1,18 @@
+import java.util.Arrays;
 public class CheckSortedArray {
     static boolean result(int arr[]) {
-     
-      
+        Arrays.sort(arr);
         for (int i = 0; i < arr.length-1; i++) {
           if(arr[i]>arr[i+1]){
             return false;
         }
             
-   
         }
         return true;
     }
 
     public static void main(String[] args) {
-        int arr[] = { 21, 22,23,24,25 };
+        int arr[] = { 5,4,3,2,1};
         boolean res = result(arr);
         System.out.println(res);
     }
