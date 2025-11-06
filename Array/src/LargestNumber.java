b7d0e4ef73baa530edc9e94940fde7359b2b5a00
Ashutosh Ayush <jbnashu297@gmail.com>
2025-11-06 13:00:22 +0530

    Largest Number

--- Array/src/LargestNumber.java
+++ Array/src/LargestNumber.java
@@ -1,9 +1,15 @@
 public class LargestNumber {
-    int Largest(int arr[]){
+   static int Largest(int arr[]){
         int max=arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
         return max;
     }
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int result=Largest(arr);
+        System.out.println(result);
     }
 }
