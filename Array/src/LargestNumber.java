ce4341d468a0295107e55932f701c02f844d6939
Ashutosh Ayush <jbnashu297@gmail.com>
2025-11-06 12:58:29 +0530

    Largest Number

---
+++ Array/src/LargestNumber.java
@@ -0,0 +1,9 @@
+public class LargestNumber {
+    int Largest(int arr[]){
+        int max=arr[0];
+        return max;
+    }
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
