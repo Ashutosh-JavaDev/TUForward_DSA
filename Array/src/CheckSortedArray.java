7c7ecbe447417f1cfe7d5678952f75a50f4ad635
Ashutosh Ayush <jbnashu297@gmail.com>
2025-11-06 17:13:56 +0530

    Check Sorted Array

--- Array/src/CheckSortedArray.java
+++ Array/src/CheckSortedArray.java
@@ -1,19 +1,23 @@
 public class CheckSortedArray {
-    static boolean result(int arr[]){
-        boolean ans=true;
-        for(int i=0;i<arr.length;i++){
-            if((arr[i]>arr[i+1])||(arr[i]<arr[i+1])){
-                ans=true;
+    static boolean result(int arr[]) {
+        boolean ans = true;
+        int j = 1;
+        for (int i = 0; i < arr.length; i++) {
+            if ((arr[i] > arr[j]) || arr[i] < arr[j]) {
+                ans = true;
+                j++;
+            } else {
+                ans = false;
+                break;
             }
-            else{
-                ans=false;
-            }
+            
         }
         return ans;
     }
+
     public static void main(String[] args) {
-        int arr[]={21,23,42,13,42};
-        boolean res=result(arr);
+        int arr[] = { 21, 23, 42, 13, 42 };
+        boolean res = result(arr);
         System.out.println(res);
     }
 }
