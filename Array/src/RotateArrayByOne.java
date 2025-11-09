1199c6989ecd767ab50d9c6f27d15673b5f526f7
Ashutosh Ayush <jbnashu297@gmail.com>
2025-11-10 01:25:29 +0530

    Rotated Array

--- Array/src/RotateArrayByOne.java
+++ Array/src/RotateArrayByOne.java
@@ -7,19 +7,17 @@ public class RotateArrayByOne {
             System.out.println(arr[0]);
         }
         else{
-            for(int i=0;i<arr.length;i++){
-                
-                arr[i]=arr[i+1];
+            for(int i=1;i<arr.length;i++){
                 System.out.print(arr[i]+ " ");
             }
-            System.out.println(arr[arr.length-1]=arr[0]);
-
+            System.out.print(arr[0]);
         }
+        System.out.println();
        
     }
     public static void main(String[] args) {
         int arr[]={1,2,3,4,5};
         rotatedArray(arr);  
-        System.out.println(arr);
+        // System.out.println(arr);
     }
 }
