70ecf80a3ad09505897c62564994f3dd6210c14c
Ashutosh Ayush <jbnashu297@gmail.com>
2025-11-10 03:29:46 +0530

    Nth time Rotated Array

--- Array/src/NthTimeRotate.java
+++ Array/src/NthTimeRotate.java
@@ -16,4 +16,11 @@ public class NthTimeRotate {
             arr[arr.length-1]=first;
         }
     }
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6};
+        rotate_N_Array(arr, 6);
+        for(int val:arr){
+            System.out.println(val);
+        }
+    }
 }
