23f5cb1676b6a10298f2584ead52b436cd592e95
Ashutosh Ayush <jbnashu297@gmail.com>
2025-11-10 02:31:26 +0530

    Rotated Array

--- Array/src/RotateArrayByOne.java
+++ Array/src/RotateArrayByOne.java
@@ -1,24 +1,28 @@
-import java.util.Arrays;
-
 public class RotateArrayByOne {
     // Input 1,2,3,4,5
     // Output 2,3,4,5,1
     public static void rotatedArray(int[] arr){
-       String str=Arrays.toString(arr);
-        if(str.length()==1){
-            System.out.print(str.charAt(0));
+       
+        if(arr==null||arr.length<=1){
+            return;
         }
         else{
-            for(int i=1;i<str.length();i++){
-                System.out.print(str.charAt(i)+ " ");
+            int temp=arr[0];
+            for(int i=1;i<arr.length;i++){
+                arr[i-1]=arr[i];
             }
-            System.out.print(str.charAt(0));
+            arr[arr.length-1]=temp;
         }
-        System.out.println();
+       
+       
     }
     public static void main(String[] args) {
         int arr[]={-1,0,3,6};
         rotatedArray(arr);  
+
+        for(int val:arr){
+            System.out.print(val+" ");
+        }
        
     }
 }
