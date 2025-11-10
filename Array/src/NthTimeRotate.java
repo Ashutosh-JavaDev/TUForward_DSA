256d5129e13683d07d304a789f9f5f94149c26ef
Ashutosh Ayush <jbnashu297@gmail.com>
2025-11-10 16:10:51 +0530

    Nth Rotated Array

--- Array/src/NthTimeRotate.java
+++ Array/src/NthTimeRotate.java
@@ -30,11 +30,26 @@ public static void functionCall(int arr[],int k){
     if(n<=1||k%n==0){
         return;
     }
+    k%=n;
+    reverse(arr, 0, k-1);
+    reverse(arr, k, arr.length-1);
+
+    reverse(arr, 0, n-1);
 }
 
+public static void reverse(int arr[],int start,int end){
+    while(start<end){
+        int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+        start++;
+        end--;
+    }
+}
     public static void main(String[] args) {
         int arr[] = { 1, 2, 3, 4, 5, 6 };
-        rotate_N_Array(arr, 10);
+        // rotate_N_Array(arr, 10);
+        functionCall(arr, 8);
         // s(arr, 10, 1);
         for (int val : arr) {
             System.out.print(val + " ");
