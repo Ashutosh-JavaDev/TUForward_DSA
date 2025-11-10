326413ef69bfb7790bb376cf6301936a7f31e1bd
Ashutosh Ayush <jbnashu297@gmail.com>
2025-11-11 01:16:26 +0530

    Merge Union

--- Array/src/MergeSortedArray.java
+++ Array/src/MergeSortedArray.java
@@ -1,12 +1,29 @@
-import java.util.Arrays;
+
 import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class MergeSortedArray {
     public static void merge(int one[], int two[]) {
         HashSet<Integer> set = new HashSet<>();
         int value=one.length+two.length;
        int three[]=new int[value];
-       
+       int k=0;
+       for(int i=0;i<one.length;i++){
+        three[k]=one[i];
+        set.add(three[k]);
+        k++;
+       }
+       for(int i=0;i<two.length;i++){
+        three[k]=two[i];
+        set.add(three[k]);
+        k++;
+       }
+       System.out.print(set+" ");
+         Set<Integer> sortedSet = new TreeSet<>(set);
+         System.out.println();
+       System.out.print(sortedSet+" ");
+
     }
 
     public static void main(String[] args) {
