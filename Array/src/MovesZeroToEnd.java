888582d59fa8716c0a56f08512db1e54fcecfaff
Ashutosh Ayush <jbnashu297@gmail.com>
2025-11-10 17:43:18 +0530

    Nth Rotated Array

--- Array/src/MovesZeroToEnd.java
+++ Array/src/MovesZeroToEnd.java
@@ -1,16 +1,19 @@
 public class MovesZeroToEnd {
     public static void MoveZero(int arr[]){
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==0&&i!=arr.length-1){
-                int temp=arr[i];
-                arr[i]=arr[i+1];
-                arr[i+1]=temp;
+        if(arr==null|| arr.length==0) return;
+      int x=0;
+
+      for(int num:arr){
+            if(num!=0){
+                arr[x++]=num;
             }
-           
-        }
+      }
+      while(x<arr.length){
+        arr[x++]=0;
+      }
     }
     public static void main(String[] args) {
-        int arr[]={1,2,0,4,0};
+        int arr[]={0,2,0,4,3,0};
         MoveZero(arr);
         for(int val:arr){
             System.out.print(val+" ");
