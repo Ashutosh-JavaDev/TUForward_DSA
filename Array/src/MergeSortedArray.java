af73fdc872ab114308d78d868ca04c561935fb4d
Ashutosh Ayush <jbnashu297@gmail.com>
2025-11-11 00:04:20 +0530

    Linear Search

---
+++ Array/src/MergeSortedArray.java
@@ -0,0 +1,11 @@
+import java.util.HashSet;
+
+public class MergeSortedArray {
+    public static void merge(int one[],int two[]){
+        HashSet <Integer>set=new HashSet<>();
+        int x=one.length+two.length;
+        for(int i=0;i<x;i++){
+            
+        }
+    }    
+}
