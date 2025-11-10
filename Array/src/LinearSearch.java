745c445d6514948b4e3118b384b14d8dceb6a2cd
Ashutosh Ayush <jbnashu297@gmail.com>
2025-11-10 23:40:59 +0530

    Linear Search

--- Array/src/LinearSearch.java
+++ Array/src/LinearSearch.java
@@ -7,4 +7,9 @@ public class LinearSearch {
     }
     return -1;
  }   
+ public static void main(String[] args) {
+    int arr[]={32,132,32,13,423};
+    int res=searching(arr, 32);
+    System.out.print(res+1);
+ }
 }
