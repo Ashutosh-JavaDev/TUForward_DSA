82bd5f342c26981a74e69a85f7a8f3b7ef57c8ef
Ashutosh Ayush <jbnashu297@gmail.com>
2025-11-12 01:50:28 +0530

    Find Missing Number

--- Array/src/FindthemissingNumber.java
+++ Array/src/FindthemissingNumber.java
@@ -1,7 +1,10 @@
 public class FindthemissingNumber {
     public static int missing(int arr[]){
         int num=0;
-        
+        for(int i=0;i<arr.length;i++){
+
+            
+        }
         return num;
     }
 }
