0631c140aa683f755bf9d87fd981bc64dd90d22d
Ashutosh Ayush <jbnashu297@gmail.com>
2025-11-13 00:40:50 +0530

    Maximum Consutive

--- Array/src/MaximumConsutive.java
+++ Array/src/MaximumConsutive.java
@@ -1,26 +1,26 @@
 public class MaximumConsutive {
     public static int Count(int arr[]) {
-        int count = 0;
+        int max = 0,currentMax=0;
         for (int i = 0; i < arr.length; i++) {
 
             if (arr[i] > 1 || arr[i] < 0) {
                 return -1;
-            } else if (arr[i] != 1) {
-                return 0;
             } else if (arr[i] == 0) {
-                count = 0;
-                
+                currentMax = 0;
                 continue;
             } else if (arr[i] == 1) {
-                count++;
-                
+                currentMax++;
+                max=currentMax;
+            }
+            else{
+                return 0;
             }
         }
-        return count;
+        return max;
     }
 
     public static void main(String[] args) {
-        int arr[] = { 1, 0, 0, 1, 1, 1, 0 };
+        int arr[] = { 0, 0 ,0,0,0};
         int res = Count(arr);
         System.out.println(res);
     }
