3e0fd6a06474e7084dc72b7d27912131094da820
Ashutosh Ayush <jbnashu297@gmail.com>
2025-11-12 23:43:26 +0530

    Find Missing Number

--- Array/src/FindthemissingNumber.java
+++ Array/src/FindthemissingNumber.java
@@ -10,17 +10,11 @@ public class FindthemissingNumber {
         int miss=actual-sum;
         return miss;
     }
-    public static int miss(int arr[]){
-        int val=Arrays.stream(arr).sum();
-        return val;
-        // System.out.println(arr);
-    }
+   
     public static void main(String[] args) {
         int arr[]={0,1,3};
         int res=missingNumber(arr);
         System.out.println(res);
-        // int res=miss(arr);
-        // System.out.println(res);
-        // miss(arr);
+       
     }
 }
