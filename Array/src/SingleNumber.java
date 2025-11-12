97bfe778a9551f33c197fde71f765c8ea7f95993
Ashutosh Ayush <jbnashu297@gmail.com>
2025-11-13 02:34:19 +0530

    Single number found

--- Array/src/SingleNumber.java
+++ Array/src/SingleNumber.java
@@ -1,26 +1,32 @@
 import java.util.HashSet;
+import java.util.Set;
 
 public class SingleNumber {
-    public static HashSet<Integer> check(int arr[]){
-        HashSet<Integer>single=new HashSet<>();
-       
-     HashSet <Integer>dupli=new HashSet<>();
-        if(arr.length%2==0){
+    public static int check(int arr[]) {
+        HashSet<Integer> single = new HashSet<>();
+
+        HashSet<Integer> dupli = new HashSet<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (!single.add(arr[i])) {
+                dupli.add(arr[i]);
+            }
+
+        }
+        Set<Integer> onlyInSet1 = new HashSet<>(single);
+
+        onlyInSet1.removeAll(dupli);
+        if (onlyInSet1.isEmpty()) {
             return -1;
         }
-        else{
-            for(int i=0;i<arr.length;i++){
-                   if(!single.add(arr[i])){
-                    dupli.add(arr[i]);
-                   }
 
-            }
-        }
-        return single;
-    }   
+        return onlyInSet1.iterator().next();
+
+    }
+
     public static void main(String[] args) {
-        int arr[]={1,2,1,3,4,5,2,4,5};
-        HashSet <Integer>res=check(arr);
+        int arr[] = { 1, 2, 2,4,3,1,4 };
+        int res = check(arr);
         System.out.println(res);
     }
 }
