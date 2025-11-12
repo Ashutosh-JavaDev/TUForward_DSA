281482ad7868ededb9b91050ba9cc73dc9f1cc0f
Ashutosh Ayush <jbnashu297@gmail.com>
2025-11-13 02:11:43 +0530

    Single number found

--- Array/src/SingleNumber.java
+++ Array/src/SingleNumber.java
@@ -1,7 +1,26 @@
+import java.util.HashSet;
+
 public class SingleNumber {
-    public static int check(int arr[]){
-        if(arr.length-1%2==0){
+    public static HashSet<Integer> check(int arr[]){
+        HashSet<Integer>single=new HashSet<>();
+       
+     HashSet <Integer>dupli=new HashSet<>();
+        if(arr.length%2==0){
             return -1;
         }
+        else{
+            for(int i=0;i<arr.length;i++){
+                   if(!single.add(arr[i])){
+                    dupli.add(arr[i]);
+                   }
+
+            }
+        }
+        return single;
     }   
+    public static void main(String[] args) {
+        int arr[]={1,2,1,3,4,5,2,4,5};
+        HashSet <Integer>res=check(arr);
+        System.out.println(res);
+    }
 }
