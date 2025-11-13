eb685d1e7fab0aedab8bb55debf4f5327f94068e
Ashutosh Ayush <jbnashu297@gmail.com>
2025-11-14 00:14:47 +0530

    Sort Color

--- Array/src/SortColour.java
+++ Array/src/SortColour.java
@@ -2,9 +2,12 @@ import java.util.Arrays;
 public class SortColour {
     public static void check(int arr[]){
         Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+       
     }
     public static void main(String[] args) {
         int arr[]={1,2,0,0};
-        System.out.println(arr);
+
+        check(arr);
     }
 }
