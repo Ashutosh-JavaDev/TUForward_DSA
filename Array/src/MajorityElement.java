bbd2b7b09a1fb92faa86fd2839a698fa97e67ffa
Ashutosh Ayush <jbnashu297@gmail.com>
2025-11-16 22:26:14 +0530

    Majority element

--- Array/src/MajorityElement.java
+++ Array/src/MajorityElement.java
@@ -4,13 +4,7 @@ public class MajorityElement {
     public static int element(int arr[]){
         int count=0,number=0;
        Arrays.sort(arr);
-       for(int i=0;i<arr.length;i++){
-        for(int j=i+1;j<arr.length-1;j++){
-            if(arr[i]==arr[j]){
-                
-            }
-        }
-       }
+       
         return number;
     }
 }
