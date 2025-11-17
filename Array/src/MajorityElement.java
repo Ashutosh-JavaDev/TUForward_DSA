5dd209aa06603c934d7c2f1ffa778c235f6c9fd7
Ashutosh Ayush <jbnashu297@gmail.com>
2025-11-17 12:21:47 +0530

    Majority element

--- Array/src/MajorityElement.java
+++ Array/src/MajorityElement.java
@@ -1,31 +1,27 @@
 import java.util.Arrays;
 
 public class MajorityElement {
+
     public static int element(int arr[]) {
-        // Input: nums = [7, 0, 0, 1, 7, 7, 2, 7, 7]\\[0,0,1,2,7,7,7,7,7]
-        int count = 1, number = 0, max = 1, data = 0;
         Arrays.sort(arr);
-        int i, j = 1;
-        int n = arr.length;
-        for (i = 0; i < n; i++) {
-            if (arr[i] == arr[j]) {
-                max = count++;
+
+        int count = 1;
+        int max = 1;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == arr[i - 1]) {
+                count++;
             } else {
                 count = 1;
             }
-            data = Math.max(max, count);
-
-            j++;
-            if (j == n) {
-                break;
-            }
+            max = Math.max(max, count);
         }
-        return data;
+        return max;
     }
 
     public static void main(String[] args) {
-        int arr[] = { 7, 0, 0, 1, 7, 7, 2, 7 };
+        int arr[] = { 7, 0, 0, 1, 7, 7, 2, 7, 7 };
         int res = element(arr);
-        System.out.println("Data:" + res);
+        System.out.println("Data: " + res);  // Output: 5
     }
 }
