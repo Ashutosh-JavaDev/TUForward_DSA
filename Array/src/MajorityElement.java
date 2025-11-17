706f2fee22e8c04f3152bcc2139244189c05a6c5
Ashutosh Ayush <jbnashu297@gmail.com>
2025-11-17 11:13:54 +0530

    Majority element

--- Array/src/MajorityElement.java
+++ Array/src/MajorityElement.java
@@ -1,12 +1,17 @@
 import java.util.Arrays;
-import java.util.HashMap;
 
 public class MajorityElement {
     public static int element(int arr[]){
+        // Input: nums = [7, 0, 0, 1, 7, 7, 2, 7, 7]\\[0,0,1,2,7,7,7,7,7]
         int count=0,number=0;
        Arrays.sort(arr);
-       HashMap<Integer,Integer>data=new HashMap<>();
-       
+       int i,j=0;
+       int n=arr.length;
+       for(i=0;i<n;i++){
+        if(arr[i]==arr[j]){
+            count++;
+        }
+       }
         return number;
     }
 }
