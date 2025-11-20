c6a3fc13cd5e0a09bf6e4cb2ab7ef51be2d00b41
Ashutosh Ayush <jbnashu297@gmail.com>
2025-11-20 22:59:10 +0530

    Striver Sheet

--- Array/src/Stock.java
+++ Array/src/Stock.java
@@ -1,5 +1,20 @@
 public class Stock {
-    public static void buysell(int arr[]){
-        
+    public static boolean buysell(int arr[]) {
+        int sum = 0;
+        boolean res=true;
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]<arr[i+1]){
+                return res;
+            }
+            else{
+                res=false;
+            }
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        int arrr[]={1,2,3,4,5};
+        boolean res=buysell(arrr);
+        System.out.println(res);
     }
 }
