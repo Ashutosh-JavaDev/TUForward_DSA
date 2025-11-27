9c093431bf672dedae5fce4899b1935c797bb480
Ashutosh Ayush <jbnashu297@gmail.com>
2025-11-28 01:36:54 +0530

    remove duplicate

--- Array/src/RemoveDuplicate.java
+++ Array/src/RemoveDuplicate.java
@@ -1,12 +1,13 @@
 import java.util.Arrays;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 
 public class RemoveDuplicate {
     static void RemoveDuplicate(Integer arr[]){
-        HashSet<Integer>set=new HashSet<>(Arrays.asList(arr));
+        LinkedHashSet<Integer>set=new LinkedHashSet<>(Arrays.asList(arr));
+  System.out.println(set);
     }
     public static void main(String[] args) {
-        Integer arr[]={1,2,3,2,3,4,2,4};
+        Integer arr[]={1,2,3,2,5,5,3,4,2,4};
         RemoveDuplicate(arr);
     }
 }
