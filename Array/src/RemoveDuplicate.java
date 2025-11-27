55a1031537616f687a04ea6c392c56ceca4681d4
Ashutosh Ayush <jbnashu297@gmail.com>
2025-11-27 13:42:13 +0530

    find duplicate

--- Array/src/RemoveDuplicate.java
+++ Array/src/RemoveDuplicate.java
@@ -1,3 +1,9 @@
 public class RemoveDuplicate {
-    
+    static void RemoveDuplicate(int arr[]){
+
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,3,2,3,4,2,4};
+        RemoveDuplicate(arr);
+    }
 }
