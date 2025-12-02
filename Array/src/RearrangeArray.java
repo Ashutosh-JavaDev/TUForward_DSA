d8685c82f948a4c77b60da025c6c70a57462d467
Ashutosh Ayush <jbnashu297@gmail.com>
2025-12-02 23:43:52 +0530

    rearrange array

--- Array/src/RearrangeArray.java
+++ Array/src/RearrangeArray.java
@@ -1,8 +1,16 @@
 import java.util.Arrays;
 public class RearrangeArray {
     public static int[] rearrange(int arr[]){
-        int newArr[]={};
+        int newArr[]=new int[arr.length-1],j=0;
         Arrays.sort(arr);        
+        for(int i=0;i<arr.length-1;i++){
+            if(i%2==0){
+                if(arr[i]>0){
+                    newArr[j]=arr[i];
+                }
+               
+            }
+        }
         return newArr;
     }
 }
