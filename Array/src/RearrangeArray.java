3cfdf4b97ba4f4eab57c52fabc8111bc4c1d4395
Ashutosh Ayush <jbnashu297@gmail.com>
2025-12-04 02:05:56 +0530

    Succesfully connected

--- Array/src/RearrangeArray.java
+++ Array/src/RearrangeArray.java
@@ -1,25 +1,36 @@
 import java.util.Arrays;
+
 public class RearrangeArray {
-    public static int[] rearrange(int arr[]){
-        int newArr[]=new int[arr.length-1],j=0;
-        Arrays.sort(arr);        
-        for(int i=0;i<arr.length-1;i++){
-            if(i%2==0){
-                if(arr[i]>0){
-                    newArr[j]=arr[i];
+    public static int[] rearrange(int arr[]) {
+        int newArr[] = new int[arr.length];
+        Arrays.sort(arr);
+
+        int posIndex = 1;
+        int negIndex = 0;
+        for (int newNum : arr) {
+            if (newNum < 0) {
+                if (negIndex < arr.length) {
+                    newArr[negIndex] = newNum;
+                    negIndex += 2;
+                }
+            } else {
+                if (posIndex < arr.length) {
+                    newArr[posIndex] = newNum;
+                    posIndex += 2;
                 }
             }
-            else{
-                if(arr[i]<0){
-                    newArr[j]=arr[i];
-                }
-            }
-            j++;
         }
         return newArr;
+
     }
+
     public static void main(String[] args) {
-        int arr[]={2,-3,4,-5,6};
-        System.out.println(rearrange(arr));
+        // -5,-3,2,4,6 || -5,2-3,4,68
+        int arr[] = { 2, -3, 4, -5, 8 };
+        int res[] = rearrange(arr);
+        for (int val : res) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
     }
 }
