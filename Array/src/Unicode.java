5f24113f175fa5c25b20500ff46d78c7aaaa0e06
Ashutosh Ayush <jbnashu297@gmail.com>
2025-12-06 12:03:51 +0530

    Unicode

--- Array/src/Unicode.java
+++ Array/src/Unicode.java
@@ -1,10 +1,10 @@
 public class Unicode {
-    public static int Unicode(String str,int index){
-        return str.codePointAt(str.charAt(index));
+    public static int Unicode(String str,int index,int inx){
+        return str.codePointCount(index,inx);
     }
     public static void main(String[] args) {
         String str="Ashutosh Ayush";
-        int res=Unicode(str, 4);
+        int res=Unicode(str, 4,8);
         System.out.println(res);
     }
 }
