9b51f2cb0ae41b6c60f676970229eb5d34720a06
Ashutosh Ayush <jbnashu297@gmail.com>
2025-12-11 23:41:05 +0530

    Armstrong Number

--- Array/src/Armstrong.java
+++ Array/src/Armstrong.java
@@ -1,24 +1,29 @@
 // import java.util.Math;
 public class Armstrong {
     public static boolean isArmstrong(int num) {
-        boolean res=true;
-        int n=num,len=0;
-        while(n!=0){
-            n=n/10;
-            len=len+1;
+        boolean res = true;
+        int n = num, len = 0;
+        while (n != 0) {
+            n = n / 10;
+            len = len + 1;
         }
-        int rev=0,val=num;
-        double dig=0,sum=0;
-        while(val!=0){
-            rev=val%10;
-           dig=Math.pow(rev, len);
-           sum=sum+dig;
-            val=val/10;
+        int rev = 0, val = num;
+        double dig = 0, sum = 0;
+        while (val != 0) {
+            rev = val % 10;
+            dig = Math.pow(rev, len);
+            sum = sum + dig;
+            val = val / 10;
         }
-        if(sum==num){
+        if (sum == num) {
             return true;
         }
         return false;
 
+    }
+    public static void main(String[] args) {
+        int num=153;
+        boolean res=isArmstrong(num);
+        System.out.println(res);
     }
 }
